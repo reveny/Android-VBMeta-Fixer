e66d2e891a97d82b3cda90540af2b0bbdb2de400
Reveny <113244907+reveny@users.noreply.github.com>
2024-11-25 11:25:30 +0100

    Fixed issues in PR

--- module/service.sh
+++ module/service.sh
@@ -2,6 +2,12 @@ until [ "$(getprop sys.boot_completed)" = "1" ]; do
     sleep 1
 done
 
+# Define paths
+BOOT_HASH_FILE="/data/data/com.reveny.vbmetafix.service/cache/boot.hash"
+TARGET="/data/adb/tricky_store/target.txt"
+timeout=10
+counter=0
+
 # Wait until we are in the launcher
 while true; do
     current_focus=$(dumpsys window | grep -E "mCurrentFocus")
@@ -16,38 +22,26 @@ done
 
 # Delay for 10 seconds which is hopefully enough
 sleep 10
+
 # Remove old to prevent getting applied if generation failed.
 rm -rf $BOOT_HASH_FILE
+
+# Add to target.txt if not already present
+if ! grep -q "com.reveny.vbmetafix.service" "$TARGET"; then
+    sed -i -e ':a' -e '/^\n*$/{$d;N;};/\n$/ba' "$TARGET"
+    echo "com.reveny.vbmetafix.service" >> "$TARGET"
+fi
+
 # Run the service
 am start-foreground-service -n com.reveny.vbmetafix.service/.FixerService --user 0 </dev/null 1>/dev/null 2>&1
-
 echo "vbmeta-fixer: service.sh - service started" >> /dev/kmsg
-
-# Define paths
-BOOT_HASH_FILE="/data/data/com.reveny.vbmetafix.service/cache/boot.hash"
-TARGET="/data/adb/tricky_store/target.txt"
-timeout=10
-counter=0
 
 # Attempt to read the boot hash file until it's available or timeout is reached
 while [ $counter -lt $timeout ]; do
     if [ -f "$BOOT_HASH_FILE" ]; then
         boot_hash=$(cat "$BOOT_HASH_FILE")
-        if [ "$boot_hash" = "null" ]; then
-            # Check if the target file contains the service
-            if [ -f "$TARGET" ]; then
-                if ! grep -q "com.reveny.vbmetafix.service" "$TARGET"; then
-                    sed -i -e ':a' -e '/^\n*$/{$d;N;};/\n$/ba' "$TARGET"
-                    echo "com.reveny.vbmetafix.service" >> "$TARGET"
-                    sleep 1
-                    am start-foreground-service -n com.reveny.vbmetafix.service/.FixerService --user 0 </dev/null 1>/dev/null 2>&1
-                    sleep 1
-                    boot_hash=$(cat "$BOOT_HASH_FILE")
-                    if [ "$boot_hash" = "null" ]; then
-                    boot_hash=""
-                    fi
-                fi
-            fi
+        if [ "$boot_hash" == "null" ]; then
+            boot_hash=""
         fi
         resetprop ro.boot.vbmeta.digest "$boot_hash"
         
